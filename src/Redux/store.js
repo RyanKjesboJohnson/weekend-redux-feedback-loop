c24107f87c9432759476a3cbcd862c9285a77c92
Ryan Kjesbo-Johnson <Ryanmark86@gmail.com>
2023-12-02 20:07:25 -0600

    Completed post and clear functionality

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,14 +3,22 @@ import { createStore, combineReducers, applyMiddleware, appReducer } from 'redux
 
 const feelingReducer = (state = '', action) => {
     if(action.type === 'ADD_FEELING'){
-      const feelingParameter = action.payload;
-      return feelingParameter;
+        const feelingParameter = action.payload;
+        return feelingParameter;
   }
+    if(action.type === 'CLEAR'){
+        const feelingParameter = '';
+        return feelingParameter;
+}
     return state;}
 
 const understandingReducer = (state = '', action) => {
     if(action.type === 'ADD_UNDERSTANDING'){
     const understandingParameter = action.payload;
+    return understandingParameter;
+}
+if(action.type === 'CLEAR'){
+    const understandingParameter = '';
     return understandingParameter;
 }
 return state;
@@ -22,6 +30,10 @@ const supportReducer = (state = '', action) => {
     const supportParameter = action.payload;
     return supportParameter;
 }
+    if(action.type === 'CLEAR'){
+        const supportParameter = '';
+        return supportParameter;
+    }
   return state;
 }
 
@@ -31,6 +43,10 @@ const commentsReducer = (state = '', action) => {
     const commentsParameter = action.payload;
     return commentsParameter
 }
+    if(action.type === 'CLEAR'){
+        const commentParameter = '';
+        return commentParameter;
+    }
 
   return state;
 }
